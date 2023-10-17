11fbfd4817c57e5ef5684ba38f658009d845e322
burgokmen <burgokmen@gmail.com>
2023-10-18 01:01:55 +0300

    init

--- DependencyInjection/src/main/java/com/brutech/DependencyInjection/DependencyInjectionApplication.java
+++ DependencyInjection/src/main/java/com/brutech/DependencyInjection/DependencyInjectionApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DependencyInjectionApplication.class, args);
 	}
