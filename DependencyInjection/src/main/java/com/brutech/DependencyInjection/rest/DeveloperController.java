064e30fe2d9c2a588c94c1532ab3dd856d2e342c
burgokmen <burgokmen@gmail.com>
2023-10-18 06:49:43 +0300

    dev controller ongoing

--- DependencyInjection/src/main/java/com/brutech/DependencyInjection/rest/DeveloperController.java
+++ DependencyInjection/src/main/java/com/brutech/DependencyInjection/rest/DeveloperController.java
@@ -1,13 +1,34 @@
 package com.brutech.DependencyInjection.rest;
 
 import com.brutech.DependencyInjection.model.Developer;
+import com.brutech.DependencyInjection.tax.Taxable;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/developers")
 public class DeveloperController {
     private Map<Integer, Developer> developers;
+    private Taxable taxable;
+
+    @PostConstruct
+    public void init() {
+        developers = new HashMap<>();
+    }
+
+    @Autowired
+    public void setTaxable(Taxable taxable) {
+        this.taxable = taxable;
+    }
+
+
+
+
 
 
 
